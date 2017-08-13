ad00a03f1c436e2c23986edf83a3b34757b14ebc
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-13 16:09:46 +0800

    Updated test.

--- proplist-test.js
+++ proplist-test.js
@@ -1,6 +1,6 @@
 const assert = require( "assert" );
 const proplist = require( "./proplist.js" );
 
-assert.equal( typeof proplist( { "name": "simple" } ) == "object", true, "should be true" );
+assert.equal( typeof proplist( { "name": "simple" } ) == "object", true, "should be of object data type" );
 
 console.log( "ok" );
