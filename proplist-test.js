4353d0ffec7555e6f2aa1ac7140d4aab33bf0752
Vinse Vinalon <vinsevinalon@gmail.com>
2017-08-29 04:17:35 +0800

    Update client/server unit test.

--- proplist-test.js
+++
@@ -1,6 +0,0 @@
-const assert = require( "assert" );
-const proplist = require( "./proplist.js" );
-
-assert.equal( typeof proplist( { "name": "simple" } ) == "object", true, "should be of object data type" );
-
-console.log( "ok" );
